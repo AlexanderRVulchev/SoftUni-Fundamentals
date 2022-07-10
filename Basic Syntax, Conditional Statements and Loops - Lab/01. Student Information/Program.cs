4ef2a918f7598580ce7e8a551fb07b1092e0bf8c
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-10 08:37:29 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/01. Student Information/Program.cs
@@ -0,0 +1,22 @@
+﻿//Create a program that receives 3 lines of input:
+//•	student name 
+//•	age
+//•	average grade. 
+//Your task is to print all the info about the student in the following format:
+//"Name: {student name}, Age: {student age}, Grade: {student grade}".
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        double grade = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}");
+    }
+}
