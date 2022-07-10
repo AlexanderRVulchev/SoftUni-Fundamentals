7f4cf7286836f6f1df03ba3cd80ad66aaa4b097f
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-10 08:38:43 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/03. Passed or Failed/Program.cs
@@ -0,0 +1,20 @@
+﻿//Modify the program from the previous problem, so it will print "Failed!" if the grade is lower than 3.00.
+//Input
+//The input comes as a single double number.
+//Output
+//The output is either "Passed!" if the grade is more than 2.99, otherwise you should print "Failed!".
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+        if (grade >= 3.00) Console.WriteLine("Passed!");
+        else Console.WriteLine("Failed!");
+    }
+}
