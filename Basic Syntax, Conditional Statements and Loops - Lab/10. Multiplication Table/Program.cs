5c57f57ce0354ab700eb1c742e1958803498ebe5
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-11 07:45:29 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,16 @@
+﻿//Create a program that receives an integer as an input. Print the 10 times table for this integer.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{n} X {i} = {n * i}");
+        }
+    }
+}
