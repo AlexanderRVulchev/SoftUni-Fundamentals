3ab053bed1e9a83b8f9f60fee9ea5a4343bb75b5
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-11 07:45:17 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a program that prints on a new line the next n odd numbers (starting from 1).
+//On the last row prints the sum of all n odd numbers.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i + i - 1);
+        }
+        Console.WriteLine($"Sum: {n * n}");
+    }
+}
