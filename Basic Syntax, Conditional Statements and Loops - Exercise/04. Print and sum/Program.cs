4ccbbb86b2606cf49eb196add2f2bc00d552c0e6
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-12 07:58:54 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and sum/Program.cs
@@ -0,0 +1,23 @@
+﻿//You will receive two whole numbers from the console representing the start and the end of a sequence of numbers. 
+//Your task is to print two lines:
+//•	On the first line print, all numbers from the start of the sequence to the end inclusive
+//•	On the second line print the sum of the sequence in the format: "Sum: {sum}"
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = first; i <= second; i++)
+        {
+            Console.Write(i + " ");
+            sum += i;
+        }
+        Console.WriteLine($"\nSum: {sum}");
+    }
+}
