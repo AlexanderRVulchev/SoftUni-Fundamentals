f73177110a958f5bd7cd3450b219408164b5b42f
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-13 07:40:18 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a program, which receives a number – n, and prints a triangle from 1 to n.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
