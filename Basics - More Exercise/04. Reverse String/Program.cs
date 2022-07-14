abbf7a67759f3ca6bb6f852ed7254fbe1818a5cd
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-14 06:48:38 +0300

    Add files via upload

---
+++ Basics - More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,16 @@
+﻿//Create a program which reverses a string and print it on the console.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            Console.Write(input[i]);
+        }
+    }
+}
