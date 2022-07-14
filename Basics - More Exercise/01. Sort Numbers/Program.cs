29b92a3c2c193ae25e15f4c998e69afceb880ee1
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-14 06:47:55 +0300

    Add files via upload

---
+++ Basics - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create a program that receives three real numbers and sorts them in descending order.
+//Print each number on a new line.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        double x1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double x3 = double.Parse(Console.ReadLine());
+
+        double highest = Math.Max(Math.Max(x1, x2), Math.Max(x2, x3));
+        double lowest = Math.Min(Math.Min(x1, x2), Math.Min(x2, x3));
+        double middle = x1 + x2 + x3 - highest - lowest;
+
+        Console.WriteLine($"{highest}\n{middle}\n{lowest}");
+    }
+}
