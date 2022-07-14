1344607288429ccd3c018238491d2b272c32eb72
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-14 06:48:50 +0300

    Add files via upload

---
+++ Basics - More Exercise/05. Messages/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a program, which emulates typing an SMS.
+
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string output = string.Empty;
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+
+            int digit = int.Parse(input[0].ToString());
+
+            if (digit >= 8 && digit <= 9) digit = digit * 3 + 1;
+            else digit *= 3;
+            digit += 91 + input.Length - 1;
+            char letter = (char)digit;
+
+            if (digit == 91) output += " ";
+            else output += letter;
+        }
+        Console.WriteLine(output);
+    }
+}
