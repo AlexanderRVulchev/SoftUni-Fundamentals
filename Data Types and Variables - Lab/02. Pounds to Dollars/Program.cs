c7056bb44ab9a13250cae069138e4e1a0c26558f
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-15 18:14:23 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿//Create a program that converts British pounds to US dollars formatted to the 3rd decimal point.
+//1 British Pound = 1.31 Dollars
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        double pound = double.Parse(Console.ReadLine());
+        double dollar = 1.31 * pound;
+        Console.WriteLine($"{dollar:f3}");
+    }
+}
