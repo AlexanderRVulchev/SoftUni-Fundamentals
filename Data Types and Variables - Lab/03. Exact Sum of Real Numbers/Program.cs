6fe174faef1a20ee583a06f6ccacf64cbad3c403
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-15 18:14:41 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿//Create a program to enter n numbers and calculate and print their exact sum (without rounding).
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            decimal num = decimal.Parse(Console.ReadLine());
+            sum += num;
+        }
+        Console.WriteLine(sum);
+    }
+}
