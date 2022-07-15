62f0d559f17a41632dbb39da8c93ad857967e04a
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-15 18:13:03 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿//You will be given an integer that will be a distance in meters.
+//Create a program that converts meters to kilometers formatted to the second decimal point.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        double meters = double.Parse(Console.ReadLine());
+        double kilometers = meters / 1000;
+        Console.WriteLine($"{kilometers:f2}");
+    }
+}
