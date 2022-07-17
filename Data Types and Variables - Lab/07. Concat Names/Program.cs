9633c1cc203256c8a733ed447ba017d51d5c422b
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-17 08:08:57 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/07. Concat Names/Program.cs
@@ -0,0 +1,16 @@
+﻿//Read two names and a delimiter. Print the names joined by the delimiter.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = new string[3];
+        input[0] = Console.ReadLine();
+        input[2] = Console.ReadLine();
+        input[1] = Console.ReadLine();
+        Console.WriteLine(string.Join(string.Empty, input));
+    }
+}
