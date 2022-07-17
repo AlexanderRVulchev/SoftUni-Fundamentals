74f811fdefd8dc138bc5bdbc6e214064e78a4b43
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-17 08:09:09 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/08. Town Info/Program.cs
@@ -0,0 +1,18 @@
+﻿//You will be given 3 lines of input.
+//On the first line you will be given the name of the town, on the second – the population and on the third the area.
+//Use the correct data types and print the result in the following format:
+//"Town {town name} has population of {population} and area {area} square km".
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string town = Console.ReadLine();
+        string population = Console.ReadLine();
+        string area = Console.ReadLine();
+        Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+    }
+}
