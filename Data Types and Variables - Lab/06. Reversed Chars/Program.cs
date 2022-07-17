e7ea87cf200af082d5c0ae163b2cebf209126bc9
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-17 08:08:45 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/06. Reversed Chars/Program.cs
@@ -0,0 +1,14 @@
+﻿//Create a program that takes 3 lines of characters and prints them in reversed order with a space between them.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        char[] symbols = new char[3];
+        for (int i = 2; i >= 0; i--) symbols[i] = char.Parse(Console.ReadLine());
+        Console.WriteLine(String.Join(" ", symbols));
+    }
+}
