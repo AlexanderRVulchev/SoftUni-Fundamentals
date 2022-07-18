a22640294f7a10e0c428945fa61eec752ea497a9
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-18 12:39:13 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/09. Chars to String/Program.cs
@@ -0,0 +1,15 @@
+﻿//Create a program that reads 3 lines of input. On each line, you get a single character.
+//Combine all the characters into one string and print it on the console.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        double meters = double.Parse(Console.ReadLine());
+        double kilometers = meters / 1000;
+        Console.WriteLine($"{Math.Round(kilometers, 2, MidpointRounding.AwayFromZero):f2}");
+    }
+}
