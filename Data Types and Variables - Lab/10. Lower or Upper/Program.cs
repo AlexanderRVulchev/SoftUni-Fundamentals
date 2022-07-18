385c08444a4a507496ebdc8d29e45f1f604db5ba
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-18 12:39:32 +0300

    Add files via upload

---
+++ Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,13 @@
+﻿//Create a program that prints whether a given character is upper-case or lower case.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        char symbol = char.Parse(Console.ReadLine());
+        Console.WriteLine(symbol.ToString() == symbol.ToString().ToLower() ? "lower-case" : "upper-case");
+    }
+}
