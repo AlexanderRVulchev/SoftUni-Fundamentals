238c5dec16febab77f0607d232ecc3716d5c3a4f
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-19 14:08:44 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/04. Sum of Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿//Create a program, which sums the ASCII codes of n characters and prints the sum on the console.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += (int)char.Parse(Console.ReadLine());
+        Console.WriteLine($"The sum equals: {sum}");
+    }
+}
