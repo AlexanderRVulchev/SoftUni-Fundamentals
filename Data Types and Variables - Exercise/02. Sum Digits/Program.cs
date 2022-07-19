88052ef658ed9c72b5727fa602a4308646b84a59
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-19 14:08:12 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create a program that receives a single integer. Your task is to find the sum of its digits.
+//For example: 12345-> 1 + 2 + 3 + 4 + 5 = 15
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int inputNum = int.Parse(Console.ReadLine());
+        int sum = 0;
+        while (inputNum > 0)
+        {
+            sum += inputNum % 10;
+            inputNum /= 10;
+        }
+        Console.WriteLine(sum);
+    }
+}
