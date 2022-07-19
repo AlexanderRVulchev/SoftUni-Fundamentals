88233a40f56c56b1a2b939c75898a70d47152648
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-19 14:08:30 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/03. Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿//Calculate how many courses will be needed to elevate n persons by using an elevator of the capacity of p persons.
+//The input holds two lines: the number of people n and the capacity p of the elevator.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int people = int.Parse(Console.ReadLine());
+        int capacity = int.Parse(Console.ReadLine());
+        int courses = people / capacity;
+        if (people % capacity != 0) courses++;
+        Console.WriteLine(courses);
+    }
+}
