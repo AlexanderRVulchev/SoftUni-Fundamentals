5750dc5c77dcc7664715141981882cc7abb8621f
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-19 14:09:11 +0300

    Add files via upload

---
+++ Data Types and Variables - Exercise/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,23 @@
+﻿//Create a program that receives an integer n
+//and print all triples of the first n small Latin letters, ordered alphabetically:
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int first = 97; first < 97 + n; first++)
+        {
+            for (int second = 97; second < 97 + n; second++)
+            {
+                for (int third = 97; third < 97 + n; third++)
+                {
+                    Console.WriteLine($"{(char)first}{(char)second}{(char)third}");
+                }
+            }
+        }
+    }
+}
