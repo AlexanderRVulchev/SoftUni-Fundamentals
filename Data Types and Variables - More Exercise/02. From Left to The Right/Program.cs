635141d68e3073a874fc6a47e81e3045be2bde60
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-24 18:46:02 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/02. From Left to The Right/Program.cs
@@ -0,0 +1,28 @@
+﻿//You will receive a number that represents how many lines we will get as input.
+//On the next N lines, you will receive a string with 2 numbers separated by a single space.
+//You need to compare them. If the left number is greater than the right number,
+//you need to print the sum of all digits in the left number, otherwise,
+//print the sum of all digits in the right number.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            long[] numbers = input.Split().Select(long.Parse).ToArray();
+            long biggerNumber = numbers[0] >= numbers[1] ? numbers[0] : numbers[1];
+            string numString = biggerNumber.ToString();
+            int position = numString[0] == '-' ? 1 : 0;
+            int sum = 0;
+            for (int j = position; j < numString.Length; j++)
+                sum += int.Parse(numString[j].ToString());
+            Console.WriteLine(sum);
+        }
+    }
+}
