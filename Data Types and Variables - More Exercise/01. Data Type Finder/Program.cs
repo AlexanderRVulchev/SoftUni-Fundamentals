a2bbd02e2e203347f502461d317b745c09b6a912
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-24 18:45:11 +0300

    Add files via upload

---
+++ Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,29 @@
+﻿//You will receive input until you receive "END". Find what data type is the input. Possible data types are:
+//•	Integer 
+//•	Floating point 
+//•	Characters
+//•	Boolean
+//•	Strings
+//Print the result in the following format: "{input} is {data type} type"
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+        int typeInt;
+        double typeDouble;
+        bool typeBool;
+        char typeChar;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            if (int.TryParse(input, out typeInt)) Console.WriteLine($"{input} is integer type");
+            else if (double.TryParse(input, out typeDouble)) Console.WriteLine($"{input} is floating point type");
+            else if (bool.TryParse(input, out typeBool)) Console.WriteLine($"{input} is boolean type");
+            else if (char.TryParse(input, out typeChar)) Console.WriteLine($"{input} is character type");
+            else Console.WriteLine($"{input} is string type");
+        }
+    }
+}
