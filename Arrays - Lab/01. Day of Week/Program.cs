2c25916fa0dc33a7711118188945e53547725bc7
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-26 21:02:11 +0300

    Add files via upload

---
+++ Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,18 @@
+﻿//Enter a number in range 1-7 and print out the word representing it or "Invalid Day!". Use an array of strings.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        int n = int.Parse(Console.ReadLine());
+        if (n > 0 && n < 8)
+        {
+            Console.WriteLine(days[n - 1]);
+        }
+        else Console.WriteLine("Invalid day!");
+    }
+}
