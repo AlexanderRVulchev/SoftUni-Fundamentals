f5b842e4de583b4e5c21e6ca0d155b76b01c895b
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-26 21:03:10 +0300

    Add files via upload

---
+++ Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿//Read an array of real numbers (space separated), round them in "away from 0" style, and print the output as in the examples:
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            double number = numbers[i];
+            int rounded = (int)Math.Round(number, MidpointRounding.AwayFromZero);
+
+            Console.WriteLine($"{number} => {rounded}");
+        }
+    }
+}
