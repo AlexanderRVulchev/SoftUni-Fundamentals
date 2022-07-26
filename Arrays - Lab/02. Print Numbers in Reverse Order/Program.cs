41ee1a9d9eb1357efbd6e9568445e2e72c4cca94
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-26 21:02:58 +0300

    Add files via upload

---
+++ Arrays - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,15 @@
+﻿//Read n numbers and print them in reverse order, separated by a single space.
+
+using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = int.Parse(Console.ReadLine());
+        for (int i = n - 1; i >= 0; i--) Console.Write(arr[i] + " ");
+    }
+}
