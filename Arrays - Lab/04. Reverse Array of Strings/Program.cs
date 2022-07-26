c214562c2f758b0096d114c09a57bd795fb74203
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-26 21:03:22 +0300

    Add files via upload

---
+++ Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿//Create a program that reads an array of strings, reverses it, and prints its elements.
+//The input consists of a sequence of space-separated Strings.
+//Print the output on a single line (space separated).
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] array = Console.ReadLine().Split().ToArray();
+        string[] reversed = new string[array.Length];
+
+        for (int i = 0; i < array.Length; i++) reversed[i] = array[array.Length - 1 - i];
+        for (int i = 0; i < reversed.Length; i++) Console.Write(reversed[i] + " ");
+    }
+}
