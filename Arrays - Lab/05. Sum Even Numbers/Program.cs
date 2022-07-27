b5e28c50fd6b2d08556c0b8f43ce1c17bf11dc40
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-27 07:43:29 +0300

    Add files via upload

---
+++ Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿//Read an array from the console and sum only its even values. 
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+            if (array[i] % 2 == 0) sum += array[i];
+        Console.WriteLine(sum);
+    }
+}
