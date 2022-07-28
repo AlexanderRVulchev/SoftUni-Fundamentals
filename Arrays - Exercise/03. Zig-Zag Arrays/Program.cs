9acf8b274a6a72dc5459f82ba9f4c6e18f9d49f7
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-28 23:11:57 +0300

    Add files via upload

---
+++ Arrays - Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a program that creates 2 arrays. You will be given an integer n.
+//On the next n lines, you will get 2 integers.
+//Form 2 new arrays in a zig-zag pattern as shown below.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] array1 = new int[n];
+        int[] array2 = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int[] temp = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (i % 2 == 0) { array1[i] = temp[0]; array2[i] = temp[1]; }
+            else { array1[i] = temp[1]; array2[i] = temp[0]; }
+        }
+        for (int i = 0; i < n; i++) Console.Write(array1[i] + " ");
+        Console.WriteLine();
+        for (int i = 0; i < n; i++) Console.Write(array2[i] + " ");
+    }
+}
