0f70f698f40e19ff39b5acb917c4e1234f07d814
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-28 23:11:40 +0300

    Add files via upload

---
+++ Arrays - Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,21 @@
+﻿//Create a program that prints out all common elements in two arrays.
+//You have to compare the elements of the second array to the elements of the first.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] array1 = Console.ReadLine().Split().ToArray();
+        string[] array2 = Console.ReadLine().Split().ToArray();
+        for (int i = 0; i < array2.Length; i++)
+        {
+            for (int j = 0; j < array1.Length; j++)
+            {
+                if (array2[i] == array1[j]) Console.Write(array2[i] + " ");
+            }
+        }
+    }
+}
