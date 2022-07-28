b862bd89821a7416257dd25dd6fb9c0d180acb6a
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-28 23:12:10 +0300

    Add files via upload

---
+++ Arrays - Exercise/04. Array Rotation/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a program that receives an array and several rotations that you have to perform.
+//The rotations are done by moving the first element of the array from the front to the back.
+//Print the resulting array.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int rotation = 0; rotation < n; rotation++)
+        {
+            int temp = arr[0];
+            for (int step = 0; step < arr.Length - 1; step++) arr[step] = arr[step + 1];
+            arr[arr.Length - 1] = temp;
+        }
+
+        for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+}
