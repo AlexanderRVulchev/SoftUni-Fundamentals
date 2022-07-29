b46bdb358c87354ffc298122d03f5e0d768a942b
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-29 07:36:11 +0300

    Add files via upload

---
+++ Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿//Create a program that finds the longest sequence of equal elements in an array of integers.
+//If several equal sequences are present in the array, print out the leftmost one.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int startIndex = 0;
+        int length = 0;
+        int bestIndex = 0;
+        int bestLength = 0;
+
+        for (int current = 0; current < arr.Length - 1; current++)
+        {
+            if (arr[current] == arr[current + 1])
+            {
+                if (length == 0) { startIndex = current; length++; }
+                else length++;
+                if (length > bestLength) { bestIndex = startIndex; bestLength = length; }
+            }
+            else length = 0;
+        }
+
+        for (int i = bestIndex; i <= bestIndex + bestLength; i++)
+            Console.Write(arr[i] + " ");
+    }
+}
