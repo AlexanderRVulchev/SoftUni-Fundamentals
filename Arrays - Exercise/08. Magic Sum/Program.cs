c0da7596d5fed1bbe7fe7da350d3cfb4844484c4
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-07-30 19:36:15 +0300

    Add files via upload

---
+++ Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿//Create a program, which prints all unique pairs in an array of integers whose sum is equal to a given number.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int main = 0; main < arr.Length - 1; main++)
+            for (int secondary = main + 1; secondary < arr.Length; secondary++)
+                if (arr[main] + arr[secondary] == n) Console.WriteLine(arr[main] + " " + arr[secondary]);
+    }
+}
