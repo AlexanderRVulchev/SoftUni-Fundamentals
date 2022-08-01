d4fe00d12d6c2707869210b3237006797efc6365
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-01 18:46:39 +0300

    Add files via upload

---
+++ Arrays - More Exercise/04. Fold and Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿//Read an array of 4*k integers, fold it like shown below,
+//and print the sum of the upper and lower two rows (each holding 2 * k integers):
+
+using System;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int k = input.Length / 4;
+        int[] upperRow = new int[2 * k];
+        int[] lowerRow = new int[2 * k];
+
+        int counter = 0;
+        for (int i = k; i < k * 3; i++) lowerRow[counter++] = input[i];
+        counter = 0;
+        for (int i = k - 1; i >= 0; i--) upperRow[counter++] = input[i];
+        for (int i = 4 * k - 1; i >= 3 * k; i--) upperRow[counter++] = input[i];
+
+        for (int i = 0; i < lowerRow.Length; i++) Console.Write(lowerRow[i] + upperRow[i] + " ");
+    }
+}
