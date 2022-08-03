89b3f55bd509276282e4e0d987606e0031b5326d
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-03 18:12:43 +0300

    Add files via upload

---
+++ Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a method for printing triangles as shown below:
+
+using System;
+
+
+class Program
+{
+    static void PrintLine(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            PrintLine(1, i);
+        }
+        for (int i = n - 1; i >= 1; i--)
+        {
+            PrintLine(1, i);
+        }
+    }
+}
