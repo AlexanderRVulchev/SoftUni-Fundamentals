6cbee5737c2a9c2f0c786dde6ebf8b29deb60601
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-04 07:29:00 +0300

    Add files via upload

---
+++ Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,23 @@
+﻿//Create a method, which receives two numbers as parameters:
+//•	The first number – the base
+//•	The second number – the power
+// The method should return the base rase to the given power.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static double RaiseToPower(double x, double y)
+    {
+        return Math.Pow(x, y);
+    }
+
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        Console.WriteLine(RaiseToPower(x, y));
+    }
+}
