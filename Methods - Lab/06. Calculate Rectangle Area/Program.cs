07d003dda4d665030c63cd2776533fe1585e4c3f
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-04 07:28:34 +0300

    Add files via upload

---
+++ Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create a method that calculates and returns the area of a rectangle. 
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static double GetRectangleArea(double width, double height)
+    {
+        return width * height;
+    }
+
+    static void Main()
+    {
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+        Console.WriteLine(GetRectangleArea(width, height));
+    }
+}
