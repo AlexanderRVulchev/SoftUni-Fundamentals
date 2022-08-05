93676bdaec22654e7d4e4b241a3ce12ef4f9f666
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-05 20:22:20 +0300

    Add files via upload

---
+++ Methods - Lab/09. Greater of Two Values/Program.cs
@@ -0,0 +1,24 @@
+﻿//Create a method, which receives two numbers as parameters:
+//•	The first number – the base
+//•	The second number – the power
+// The method should return the base rase to the given power.
+
+using System;
+using System.Linq;
+
+
+class Program
+{
+    static void PrintSign(int num)
+    {
+        if (num > 0) Console.WriteLine($"The number {num} is positive.");
+        else if (num < 0) Console.WriteLine($"The number {num} is negative.");
+        else Console.WriteLine($"The number 0 is zero.");
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintSign(n);
+    }
+}
