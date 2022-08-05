c82246779671c3d59ac2d2d075f393689eec5433
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-05 20:22:32 +0300

    Add files via upload

---
+++ Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,41 @@
+﻿//You are given an input of two values of the same type. The values can be of type int, char, or string.
+//Create methods called GetMax(), which can compare int, char, or string and returns the biggest of the two values.
+
+using System;
+
+
+class Program
+{
+    static int GetSumOfEvenDigits(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            if (number % 2 == 0) sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    static int GetSumOfOddDigits(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            if (number % 2 != 0) sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    static int GetMultipleOfEvenAndOdds(int number)
+    {
+        return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+    }
+
+    static void Main()
+    {
+        int input = Math.Abs(int.Parse(Console.ReadLine()));
+        Console.WriteLine(GetMultipleOfEvenAndOdds(input));
+    }
+}
