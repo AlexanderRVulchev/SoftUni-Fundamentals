52c354188d23b62a2b939992d6fd74c98ed3156a
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-06 16:19:46 +0300

    Add files via upload

---
+++ Methods - Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a method that prints out the smallest of three integer numbers.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void PrintSmallestNumber(int a, int b, int c)
+    {
+        int smallest = Math.Min(a, b);
+        smallest = Math.Min(smallest, c);
+        Console.WriteLine(smallest);
+    }
+
+    static void Main()
+    {
+        // I just want to practice tuple variables. They're not necessary.
+        (int one, int two, int three) nums = (0, 0, 0);
+        nums.one = int.Parse(Console.ReadLine());
+        nums.two = int.Parse(Console.ReadLine());
+        nums.three = int.Parse(Console.ReadLine());
+        PrintSmallestNumber(nums.one, nums.two, nums.three);
+    }
+}
