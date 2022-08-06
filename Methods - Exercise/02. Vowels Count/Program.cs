b0b06a866f4727c8217db85c5ce99b7694c5e973
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-06 16:20:15 +0300

    Add files via upload

---
+++ Methods - Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,31 @@
+﻿//Create a method that receives a single string and prints out the number of vowels contained in it.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void PrintNumberOfVowels(string str)
+    {
+        int vowelCount = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            switch (str[i])
+            {
+                case 'a':
+                case 'e':
+                case 'o':
+                case 'i':
+                case 'u': vowelCount++; break;
+            }
+        }
+        Console.WriteLine(vowelCount);
+    }
+
+    static void Main()
+    {
+        string input = Console.ReadLine().ToLower();
+        PrintNumberOfVowels(input);
+    }
+}
