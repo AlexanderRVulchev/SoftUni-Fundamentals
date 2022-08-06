db4b4a87d8da8ac8428776596ff6ffd7c83470aa
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-06 16:20:25 +0300

    Add files via upload

---
+++ Methods - Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a method that receives two characters and prints all the characters between them according to ASCII (on a single line).
+//NOTE: If the second letter's ASCII value is less than that of the first one then the two initial letters should be swapped.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void PrintAllCharsInBetween(char first, char second)
+    {
+        for (int i = first + 1; i < second; i++)
+        {
+            Console.Write((char)i + " ");
+        }
+    }
+
+    static void Main()
+    {
+        char symbol1 = char.Parse(Console.ReadLine());
+        char symbol2 = char.Parse(Console.ReadLine());
+        if (symbol1 < symbol2) PrintAllCharsInBetween(symbol1, symbol2);
+        else PrintAllCharsInBetween(symbol2, symbol1);
+    }
+}
