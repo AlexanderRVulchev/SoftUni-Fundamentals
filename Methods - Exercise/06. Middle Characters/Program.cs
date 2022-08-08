36643f901e87eaeebd1e9b26b17c1754afa1b41e
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-08 20:12:41 +0300

    Add files via upload

---
+++ Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,22 @@
+﻿//You will receive a single string.
+//Create a method that prints the character found at its middle.
+//If the length of the string is even there are two middle characters.
+
+using System;
+
+
+class Program
+{
+    static void PrintMiddleChars(string str)
+    {
+        if (str.Length % 2 == 0)
+            Console.WriteLine($"{str[str.Length / 2 - 1]}{str[str.Length / 2]}");
+        else Console.WriteLine(str[str.Length / 2]);
+    }
+
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        PrintMiddleChars(input);
+    }
+}
