153186bf2ea2a2ee7bd7ff06244807b87de672be
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-08 20:12:28 +0300

    Add files via upload

---
+++ Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,28 @@
+﻿//You will receive 3 integers.
+//Create a method that returns the sum of the first two integers and another method
+//that subtracts the third integer from the result of the sum method.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static int SumNumbers(int x, int y)
+    {
+        return x + y;
+    }
+
+    static int SubtractNumbers(int x, int y)
+    {
+        return x - y;
+    }
+
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine(SubtractNumbers(SumNumbers(a, b), c));
+    }
+}
