af01f4f5db1f9fd3fc0ea681c22572a487749276
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-09 06:48:13 +0300

    Add files via upload

---
+++ Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a method that receives a single integer n and prints an NxN matrix using this number as a filler.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void PrintMatrix(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintMatrix(n);
+    }
+}
