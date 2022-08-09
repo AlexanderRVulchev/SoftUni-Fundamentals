2c0f0d59c30e99585a43726111f4246c30b0fe43
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-09 06:48:42 +0300

    Add files via upload

---
+++ Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a program that reads positive integers until you receive the "END" command.
+//For each number, print whether the number is a palindrome or not.
+//A palindrome is a number that reads the same backward as forward, such as 323 or 1001. 
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static bool DetermineIfPalindrome(string str)
+    {
+        for (int i = 0; i < str.Length / 2; i++)
+            if (str[i] != str[str.Length - 1 - i]) return false;
+
+        return true;
+    }
+
+    static void Main()
+    {
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+            Console.WriteLine(DetermineIfPalindrome(input));
+    }
+}
