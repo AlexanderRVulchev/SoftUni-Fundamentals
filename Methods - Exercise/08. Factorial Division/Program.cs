52d8ae0da0a536934d7263af2cf4eeb9cd825bde
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-09 06:48:29 +0300

    Add files via upload

---
+++ Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,28 @@
+﻿//Read two integers. Calculate the factorial of each number.
+//Divide the first result by the second and print the result of the division formatted to the second decimal point.
+
+using System;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static decimal GetFactorial(decimal n)
+    {
+        decimal result = 1m;
+        for (int i = 1; i <= n; i++) result *= i;
+        return result;
+    }
+
+    static decimal FactorialDivision(decimal a, decimal b)
+    {
+        return GetFactorial(a) / GetFactorial(b);
+    }
+
+    static void Main()
+    {
+        decimal first = decimal.Parse(Console.ReadLine());
+        decimal second = decimal.Parse(Console.ReadLine());
+        Console.WriteLine($"{FactorialDivision(first, second):f2}");
+    }
+}
