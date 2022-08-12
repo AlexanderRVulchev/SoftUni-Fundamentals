6cfa29c98501839534389e4a5c002a47125d5e5e
Alexander Vulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-12 07:41:45 +0300

    Add files via upload

---
+++ Methods - More Exercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,31 @@
+﻿//You are given a number num1, num2, and num3.
+//Write a program that finds if num1 * num2 * num3 (the product) is negative,
+//positive, or zero.Try to do this WITHOUT multiplying the 3 numbers.
+
+using System;
+
+
+class Program
+{
+    static bool IsItPositiveNumber(int num, bool equasionSign)
+    {
+        if (num < 0) equasionSign = equasionSign ? false : true;
+        return equasionSign;
+    }
+    static void Main()
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("zero");
+            return;
+        }
+        bool isPositive = true;
+        isPositive = IsItPositiveNumber(num1, isPositive);
+        isPositive = IsItPositiveNumber(num2, isPositive);
+        isPositive = IsItPositiveNumber(num3, isPositive);
+        Console.WriteLine(isPositive ? "positive" : "negative");
+    }
+}
