81ee13f57f09a98b219883cb74e0aa695c3eb248
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-22 07:39:47 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/02. Big Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿//You will receive a number N in the range [0…1000]. Calculate the Factorial of N and print out the result.
+
+using System;
+using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        BigInteger result = 1;
+        for (int i = 2; i <= n; i++)        
+            result *= i;
+        Console.WriteLine(result);        
+    }
+}
