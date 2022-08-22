1f559ef0250a7357048b951b55a65e6f1cd373b1
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-08-22 07:39:16 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,22 @@
+﻿//You will be given a string that will contain words separated by a single space.
+//Randomize their order and print each word on a new line.
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split().ToArray();
+        Random rnd = new Random();
+        for (int i = 0; i < input.Length; i++)
+        {
+            int swapIndex = rnd.Next(input.Length);
+            string swapValue = input[swapIndex];
+            input[swapIndex] = input[i];
+            input[i] = swapValue;
+        }
+        Console.WriteLine(String.Join("\n", input));
+    }
+}
