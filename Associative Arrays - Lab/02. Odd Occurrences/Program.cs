03697f7acfffbd8570797e91f1d3feb18fa20a1f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-06 18:16:41 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a program that extracts all elements from a given sequence of words that are present in it an odd number of times (case-insensitive).
+//•	Words are given on a single line, space-separated.
+//•	Print the result elements in lowercase, in their order of appearance.
+
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> wordsCounts = new Dictionary<string, int>();
+        string[] input = Console.ReadLine().ToLower().Split();
+        foreach (string word in input)
+        {
+            if (!wordsCounts.ContainsKey(word))
+                wordsCounts.Add(word, 0);
+            wordsCounts[word]++;
+        }        
+        foreach (KeyValuePair<string, int> word in wordsCounts)
+        {
+            if (word.Value % 2 == 1)
+            Console.Write(word.Key + " ");
+        }
+    }
+}
