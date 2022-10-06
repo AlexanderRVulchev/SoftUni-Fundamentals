d5b76355ff1fedfa7cbfef527dce0e234c135c8c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-06 18:16:11 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿//Read a list of integers and print them in ascending order, along with their number of occurrences.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+        foreach (var num in nums)
+        {
+            if (!counts.ContainsKey(num))
+                counts.Add(num, 0);
+            counts[num]++;
+        }
+        foreach (var count in counts)
+        {
+            Console.WriteLine($"{count.Key} -> {count.Value}");
+        }
+    }
+}
+
