c8464a2ab077eb3c31e7f96ffcbd737175c4a345
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-06 18:18:03 +0300

    Add files via upload

---
+++ Associative Arrays - Lab/04. Word Filter/Program.cs
@@ -0,0 +1,16 @@
+﻿//Read an array of strings and take only words, whose length is an even number. Print each word on a new line.
+
+using System;
+using System.Linq;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] words = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+        foreach (string word in words)
+            Console.WriteLine(word);
+    }
+}
+
