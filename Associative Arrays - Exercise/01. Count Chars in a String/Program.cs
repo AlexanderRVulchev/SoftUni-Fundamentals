c3be83897f1e57cae9cd49ba9fdf7e06a0421f67
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-07 18:21:08 +0300

    Add files via upload

---
+++ Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,25 @@
+﻿//Create a program that counts all characters in a string, except for space (' '). 
+//Print all the occurrences in the following format:
+//"{char} -> {occurrences}"
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        char[] chars = Console.ReadLine().Where(c => c != ' ').ToArray();
+        Dictionary<char, int> charOccurence = new Dictionary<char, int>();
+        foreach (char c in chars)
+        {
+            if (!charOccurence.ContainsKey(c))
+                charOccurence.Add(c, 0);
+            charOccurence[c]++;
+        }
+        foreach (KeyValuePair<char, int> c in charOccurence)        
+            Console.WriteLine($"{c.Key} -> {c.Value}");        
+    }
+}
