1d13d6cf00939569f0e760a3fc6942acefc9d314
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-13 20:19:21 +0300

    Add files via upload

---
+++ Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿//You will be given a series of strings until you receive an "end" command.
+//Write a program that reverses strings and prints each pair on a separate line in the format "{word} = {reversed word}".
+
+using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string word;
+        while ((word = Console.ReadLine()) != "end")
+        {            
+            StringBuilder reversedWord = new StringBuilder();
+
+            for (int i = word.Length - 1; i >= 0; i--)            
+                reversedWord.Append(word[i]);
+
+            Console.WriteLine($"{word} = {reversedWord}");
+        }
+    }
+}
