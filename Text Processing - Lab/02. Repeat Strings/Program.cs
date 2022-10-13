7a73b28525eb7de39c05c79cf5b3749a0e15ed72
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-13 20:19:55 +0300

    Add files via upload

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿//Create a Program That Reads an Array of Strings.
+//Each String is Repeated N Times, Where N is the length of the String. Print the Concatenated String.
+
+using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        StringBuilder result = new StringBuilder();
+
+        for (int wordCount = 0; wordCount < input.Length; wordCount++)
+        {
+            string word = input[wordCount];
+            for (int i = 0; i < word.Length; i++)
+                result.Append(word);
+        }
+        Console.WriteLine(result.ToString());
+    }
+}
