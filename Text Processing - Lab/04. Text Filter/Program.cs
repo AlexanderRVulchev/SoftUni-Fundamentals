9557aa918bab3410c5ae1e2730385022357f2234
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-14 21:14:47 +0300

    Add files via upload

---
+++ Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a program that takes a text and a string of banned words.
+//All words included in the ban list should be replaced with asterisks "*", equal to the word's length.
+//The entries in the ban list will be separated by a comma and space ", ".
+//The ban list should be entered on the first input line and the text on the second input line. 
+
+using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] bannedWords = Console.ReadLine().Split(", ");                
+        string bigText = Console.ReadLine();
+        
+        foreach (string word in bannedWords)
+        {
+            string replacement = new String('*', word.Length);            
+            
+            while (bigText.Contains(word))
+                bigText = bigText.Replace(word, replacement);
+        }
+
+        Console.WriteLine(bigText);
+    }
+}
