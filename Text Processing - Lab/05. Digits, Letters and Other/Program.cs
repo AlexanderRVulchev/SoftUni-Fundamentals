c57e3163b1be9f5f7e52c5735ff590e29f66fdb9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-14 21:15:28 +0300

    Add files via upload

---
+++ Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,27 @@
+﻿//Create a program that receives a single string and on the first line prints all the digits, on the second –
+//all the letters, and on the third – all the other characters.
+//There will always be at least one digit, one letter, and one other character.
+
+using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string inputString = Console.ReadLine();
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder other = new StringBuilder();
+
+        foreach (char c in inputString)
+        {
+            if (char.IsDigit(c)) digits.Append(c);
+            else if (char.IsLetter(c)) letters.Append(c);
+            else other.Append(c);
+        }
+        Console.WriteLine(digits);
+        Console.WriteLine(letters);
+        Console.WriteLine(other);
+    }
+}
