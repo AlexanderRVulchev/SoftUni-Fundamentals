08f8688aae11bd31e991bae0097226b300765979
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-16 16:44:57 +0300

    Add files via upload

---
+++ Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿//Create a program that returns an encrypted version of the same text.
+//Encrypt the text by shifting each character with three positions forward.
+//For example, A would be replaced by D, B would become E, and so on
+//. Print the encrypted text.
+
+using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        StringBuilder result = new StringBuilder();
+        
+        foreach (char c in input)
+            result.Append((char)(c + 3));
+        Console.WriteLine(result);
+    }
+}
