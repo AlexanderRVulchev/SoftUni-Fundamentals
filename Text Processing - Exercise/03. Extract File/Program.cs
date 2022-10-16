873f5ba33ced48cb0deb8bf4a22775cf17e9a041
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-16 16:44:22 +0300

    Add files via upload

---
+++ Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,16 @@
+﻿//Create a program that reads the path to a file and subtracts the file name and its extension.
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(@"\");
+        string[] tokens = input[input.Length - 1].Split('.');
+        string fileName = tokens[0];
+        string fileExtension = tokens[1];
+        Console.WriteLine($"File name: {fileName}");
+        Console.WriteLine($"File extension: {fileExtension}");
+    }
+}
