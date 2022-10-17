0d1b8d85a7fd543188cdb258b8f11e1f7e8f8b9a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-17 13:50:47 +0300

    Add files via upload

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿//Create a program that reads a string from the console and replaces any sequence of the same letter with a single corresponding letter.
+
+using System;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(Console.ReadLine());
+        int index = 0;
+        
+        while (index < sb.Length - 1)
+        {
+            if (sb[index] == sb[index + 1])
+                sb.Remove(index + 1, 1);
+            else index++;
+        }
+
+        Console.WriteLine(sb);
+    }
+}
