47ce754dd562f633ab19af0658a5fc8149c05fde
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-19 12:42:24 +0300

    Add files via upload

---
+++ Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,29 @@
+﻿//Create a program that prints a sum of all characters between two given characters (their ASCII code).
+//In the first line, you will get a character. On the second line, you get another character.
+//On the last line, you get a random string. Find all the characters between the two given and print their ASCII sum.
+
+using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        char inputChar1 = char.Parse(Console.ReadLine());
+        char inputChar2 = char.Parse(Console.ReadLine());
+
+        char lowerChar = (char)Math.Min(inputChar1, inputChar2);
+        char higherChar = (char)Math.Max(inputChar1, inputChar2);
+
+        string inputString = Console.ReadLine();
+        int sum = 0;
+
+        foreach (char c in inputString)
+        {
+            if (c > lowerChar && c < higherChar)
+                sum += c;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
