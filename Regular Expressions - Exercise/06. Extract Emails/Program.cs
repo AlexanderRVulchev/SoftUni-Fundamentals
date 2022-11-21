3c81227716ada048f3fe8f8a82ccfe5d53391f15
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-21 12:52:47 +0200

    Add files via upload

---
+++ Regular Expressions - Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        MatchCollection matchEMails = Regex.Matches(input,
+            @"\s\b[a-zA-Z\d]+([-._][a-zA-Z\d]+)*[@][a-zA-z]+([-][a-zA-z]+)*([.]([a-zA-z]+([-][a-zA-z]+)*))+");
+        Console.WriteLine(String.Join("\n", matchEMails));
+    }
+}
