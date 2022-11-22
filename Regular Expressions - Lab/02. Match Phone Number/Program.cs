f36edc6ed988322d62c446d18c222cb6b728fda7
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-22 21:06:49 +0200

    Add files via upload

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,23 @@
+﻿//Create a regular expression to match a valid phone number from Sofia.
+//After you find all valid phones, print them on the console, separated by a comma and a space ", ".
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static void Main()
+    {
+        string regex = @"\+359([ -])2\1\d{3}\1\d{4}\b";
+        string phones = Console.ReadLine();
+        
+        MatchCollection phoneMatches = Regex.Matches(phones, regex);
+        List<string> matchedPhones = phoneMatches.Cast<Match>().Select(m => m.Value).ToList();
+
+        Console.Write(String.Join(", ", matchedPhones));
+            
+    }
+}
+
